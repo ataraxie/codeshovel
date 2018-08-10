9145c39099372507384c94c22ad759be4217b957
Felix Grund <felix.grund@scandio.de>
2018-08-10 14:16:02 -0700

    env variable in MainAnalysisTask and new stub for junit4

--- src/test/java/com/felixgrund/codeshovel/MainAnalysisTask.java
+++ src/test/java/com/felixgrund/codeshovel/MainAnalysisTask.java
@@ -17,10 +17,7 @@ import java.util.Date;
 public class MainAnalysisTask {
 
 	private static final String LANG = "java";
-//	private static final String TEST_CONFIG = "checkstyle-Checker-fireErrors";
-//	private static final String TEST_CONFIG = "checkstyle-CommonUtils-createPattern";
-	private static final String TEST_CONFIG = "flink-ContinuousFileMonitoringFunction-close";
-
+	private static final String TEST_CONFIG = System.getenv("codeshovel.env.name");
 	private static final String CODESTORY_REPO_DIR = System.getenv("codeshovel.repo.dir");
 
 	public static void main(String[] args) throws Exception {
