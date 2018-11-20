affc36f170798edc90904c3d4d1a7bac14106a24
Felix Grund <felix.grund@scandio.de>
2018-11-20 16:29:13 +0100

    README and some file renaming/moving

--- src/main/java/com/felixgrund/codeshovel/execution/MainCli.java
+++ src/main/java/com/felixgrund/codeshovel/execution/MainCli.java
@@ -18,25 +18,41 @@ import org.eclipse.jgit.lib.Repository;
  */
 public class MainCli {
 
+	/*
+	Entry point for the command line tool. If options are supplied incorrectly, a help message like this will
+	be shown (printing it here because it shows the required arguments):
+
+	usage: java -jar <codeshovel-jar-file>
+		 -filepath <arg>      (required) path to the file containing the method
+		 -methodname <arg>    (required) name of the method
+		 -outfile <arg>       path to the output file. Default: current working directory
+		 -reponame <arg>      name of the repository. Default: last part from repopath (before /.git)
+		 -repopath <arg>      (required) path to the repository (on the local file system)
+		 -startcommit <arg>   hash of the commit to begin with backwards history traversal. Default: HEAD
+		 -startline <arg>     (required) start line of the method
+	 */
 	public static void main(String[] args) {
 		CommandLineParser parser = new DefaultParser();
 		Options options = new Options();
-		options.addOption(newOption("reponame", true, "name of the repository", false));
-		options.addOption(newOption("repopath", true, "path to the repository (on the local file system)", true));
-		options.addOption(newOption("filepath", true, "path to the file containing the method", true));
-		options.addOption(newOption("methodname", true, "name of the method", true));
-		options.addOption(newOption("startline", true, "start line of the method", true));
-		options.addOption(newOption("startcommit", true, "hash of the commit to begin with backwards history traversal", false));
-		options.addOption(newOption("outfile", true, "path to the output file", false));
+		options.addOption(newOption("reponame", true, "name of the repository. Default: last part from repopath (before /.git)", false));
+		options.addOption(newOption("repopath", true, "(required) path to the repository (on the local file system)", true));
+		options.addOption(newOption("filepath", true, "(required) path to the file containing the method", true));
+		options.addOption(newOption("methodname", true, "(required) name of the method", true));
+		options.addOption(newOption("startline", true, "(required) start line of the method", true));
+		options.addOption(newOption("startcommit", true, "hash of the commit to begin with backwards history traversal. Default: HEAD", false));
+		options.addOption(newOption("outfile", true, "path to the output file. Default: current working directory", false));
 
 		try {
 			CommandLine line = parser.parse(options, args);
 			String repositoryPath = line.getOptionValue("repopath");
+			// Unix vs. Windows. Probably there is a better way to do this.
 			String pathDelimiter = repositoryPath.contains("\\\\") ? "\\\\" : "/";
+			// Repo paths need to reference the .git directory. We add it to the path if it's not provided.
 			String gitPathEnding = pathDelimiter + ".git";
 			if (!repositoryPath.endsWith(gitPathEnding)) {
 				repositoryPath += gitPathEnding;
 			}
+			// If no repo name parameter was provided we extract if from the repo path.
 			String repositoryName = line.getOptionValue("reponame");
 			if (repositoryName == null) {
 				String[] split = repositoryPath.replace(gitPathEnding, "").split(pathDelimiter);
@@ -45,15 +61,18 @@ public class MainCli {
 			String filePath = line.getOptionValue("filepath");
 			String functionName = line.getOptionValue("methodname");
 			int functionStartLine = Integer.parseInt(line.getOptionValue("startline"));
+			// If no start commit hash was provided we use HEAD.
 			String startCommitName = line.getOptionValue("startcommit");
 			if (startCommitName == null) {
 				startCommitName = "HEAD";
 			}
+			// If no output file path was provided the output file will be saved in the current directory.
 			String outputFilePath = line.getOptionValue("outfile");
 			if (outputFilePath == null) {
 				outputFilePath = System.getProperty("user.dir") + "/" + repositoryName + "-" + functionName + "-" + functionStartLine + ".json";
 			}
 
+			// Below is the start of a CodeShovel execution as we know it.
 			Repository repository = Utl.createRepository(repositoryPath);
 			Git git = new Git(repository);
 			RepositoryService repositoryService = new CachingRepositoryService(git, repository, repositoryName, repositoryPath);
---
+++ src/main/java/com/felixgrund/codeshovel/execution/MainSingleStub.java
@@ -0,0 +1,56 @@
+package com.felixgrund.codeshovel.execution;
+
+import com.felixgrund.codeshovel.services.RepositoryService;
+import com.felixgrund.codeshovel.services.impl.CachingRepositoryService;
+import com.felixgrund.codeshovel.util.Utl;
+import com.felixgrund.codeshovel.wrappers.GlobalEnv;
+import com.felixgrund.codeshovel.wrappers.StartEnvironment;
+import com.google.gson.Gson;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import com.felixgrund.codeshovel.wrappers.Commit;
+
+import java.io.File;
+import java.util.Date;
+
+public class MainSingleStub {
+
+	private static final String LANG = "java";
+
+	public static void main(String[] args) throws Exception {
+		long start = new Date().getTime();
+		execute();
+		long end = new Date().getTime();
+		System.out.println("Time taken: " + (end - start) / 1000 + "sec");
+	}
+
+	public static void execute() throws Exception {
+		String configName = GlobalEnv.ENV_NAME;
+		ClassLoader classLoader = MainSingleStub.class.getClassLoader();
+		File file = new File(classLoader.getResource("stubs/" + LANG + "/" + configName + ".json").getFile());
+		String json = FileUtils.readFileToString(file, "utf-8");
+		Gson gson = new Gson();
+
+		StartEnvironment startEnv = gson.fromJson(json, StartEnvironment.class);
+		String repositoryName = startEnv.getRepositoryName();
+		String repositoryPath = GlobalEnv.REPO_DIR + "/" + repositoryName + "/.git";
+		startEnv.setRepositoryPath(repositoryPath);
+
+		Repository repository = Utl.createRepository(repositoryPath);
+		Git git = new Git(repository);
+		RepositoryService repositoryService = new CachingRepositoryService(git, repository, repositoryName, repositoryPath);
+		Commit startCommit = repositoryService.findCommitByName(startEnv.getStartCommitName());
+
+		startEnv.setStartCommit(startCommit);
+		startEnv.setEnvName(configName);
+		startEnv.setStartCommit(startCommit);
+		startEnv.setFileName(Utl.getFileName(startEnv.getFilePath()));
+
+		System.out.println("Running dynamic test for config: " + configName);
+
+		ShovelExecution.runSingle(startEnv, startEnv.getFilePath(), true);
+
+	}
+
+}
