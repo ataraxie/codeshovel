598e5d4be87b04e181b2e37e09bd4d8e57a686c5
Reid Holmes <rtholmes@gmail.com>
2021-01-29 13:47:20 -0800

    see #47 update oracle with Shaiful's version and add missing rows to elasticsearch method.

--- src/test/java/com/felixgrund/codeshovel/ThresholdRunner.java
+++ src/test/java/com/felixgrund/codeshovel/ThresholdRunner.java
@@ -3,6 +3,7 @@ package com.felixgrund.codeshovel;
 import com.felixgrund.codeshovel.util.Thresholds;
 import com.felixgrund.codeshovel.wrappers.StartEnvironment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ThresholdRunner {
@@ -10,34 +11,73 @@ public class ThresholdRunner {
         System.out.println("ThresholdRunner::init");
     }
 
+    private ArrayList<String> messages = new ArrayList<>();
+
+    /**
+     * Trying different permutations generates a lot of log messages.
+     * Cache the key messages from ThresholdRunner so they can be
+     * played back at the end of the execution.
+     *
+     * @param msg
+     */
+    public void print(String msg) {
+        System.out.println("ThresholdRunner::"+msg);
+        messages.add(msg);
+    }
     public void doIt() throws Exception {
-        System.out.println("ThresholdRunner::doIt() - start");
+        print("doIt() - start");
 
         String result = "";
         int count = 0;
 
         Thresholds.resetAll();
+        evaluateConfig(count++);
+        print("doIt() - CONFIG RESULT BASELINE, NO CHANGES");
+
+        Thresholds.BODY_SIM_MULT.setValue(1.4f);
+        Thresholds.SCOPE_SIM_MULT.setValue(0.8f);
+        Thresholds.LINE_SIM_MULT.setValue(0.6f);
+        Thresholds.NAME_SIM_MULT.setValue(1.2f);
+        evaluateConfig(count++);
+        print("doIt() - PAPER RESULT BASELINE, NO CHANGES");
+
+        // try less specific body thresholds
+        Thresholds.resetAll();
         Thresholds.MOST_SIM_FUNCTION.setValue(0.8f);
         Thresholds.MOST_SIM_FUNCTION_MAX.setValue(0.9f);
-        result = run();
-        count++;
-        System.out.println("CONFIG RESULT"+count+" complete; config: ");
-        System.out.println(Thresholds.toDiffJSON());
-        System.out.println("CONFIG RESULT "+count+": "+result);
+        evaluateConfig(count++);
 
+        // cross file within file
         Thresholds.resetAll();
-        Thresholds.MOST_SIM_FUNCTION.setValue(0.7f);
-        Thresholds.MOST_SIM_FUNCTION_MAX.setValue(0.8f);
-        result = run();
-        count++;
-        System.out.println("CONFIG RESULT"+count+" complete; config: ");
-        System.out.println(Thresholds.toDiffJSON());
-        System.out.println("CONFIG RESULT "+count+": "+result);
+        Thresholds.BODY_SIM_CROSS_FILE.setValue(0.75f);
+        Thresholds.BODY_SIM_WITHIN_FILE.setValue(0.5f);
+        evaluateConfig(count++);
 
-        System.out.println("ThresholdRunner::doIt() - done");
+        // cross file within file
+        Thresholds.resetAll();
+        Thresholds.BODY_SIM_CROSS_FILE.setValue(0.5f);
+        Thresholds.BODY_SIM_WITHIN_FILE.setValue(0.5f);
+        evaluateConfig(count++);
+
+        // cross file within file
+        Thresholds.resetAll();
+        Thresholds.BODY_SIM_CROSS_FILE.setValue(0.75f);
+        Thresholds.BODY_SIM_WITHIN_FILE.setValue(0.75f);
+        evaluateConfig(count++);
+
+        print("doIt() - ThresholdRunner::doIt() - done");
     }
+
+    private void evaluateConfig(int count) throws Exception {
+        String result = run();
+        count++;
+        print("evalConfig() - CONFIG RESULT"+count+" complete; config: ");
+        System.out.println(Thresholds.toDiffJSON());
+        print("evalConfig() - CONFIG RESULT "+count+": "+result);
+    }
+
     public String run() throws Exception {
-        System.out.println("ThresholdRunner::run() - start");
+        System.out.println("run() - ThresholdRunner::run() - start");
 
         MainDynamicStubTest runner = new MainDynamicStubTest();
         List<StartEnvironment> envs = runner.selectEnvironments();
